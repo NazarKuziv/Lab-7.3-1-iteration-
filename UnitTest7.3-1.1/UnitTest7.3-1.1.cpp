29be6d4e0e9549639600c260d96b0b811f11ea4d
Nazar Kuziv <nazar123kuziv@gmail.com>
2021-02-26 13:54:24 +0200

    UnitTest

---
+++ UnitTest7.3-1.1/UnitTest7.3-1.1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project 7.3-1(ітерація).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest7311
+{
+	TEST_CLASS(UnitTest7311)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a[3][3] = { { -1, 2, 3 },{ 4, 5, 6 },{ 7, 8, -9 } };
+			int* pa[3] = { a[0], a[1], a[2] };
+			int S = 0;
+
+			S = Sum(pa, 3, 3);
+
+			Assert::AreEqual(S, 10);
+		}
+	};
+}
